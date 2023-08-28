52a634acb920e1d267526d957ace97c5efb0ebf2
Hart <hart.hpwebdev@gmail.com>
2023-08-28 22:11:32 +0800

    Static/FE: search filter

--- app/views/tenants/index.view.php
+++ app/views/tenants/index.view.php
@@ -3,6 +3,15 @@ get_template_part('header');
 get_template_part('alerts'); 
 ?>
 
+<!-- Search Filter -->
+<div class="input-group mb-5 mx-auto" style="max-width: calc(var(--bs-breakpoint-sm) - 5rem);">
+  <label class="sr-only" for="search">Search</label>
+  <input type="search" id="search" class="form-control" placeholder="Search...">
+  <button type="button" class="btn btn-primary" aria-label="Search">
+    Search
+  </button>
+</div>
+
 <?php if (!empty($tenants)) : ?>
   <table class="table table-striped border">
       <thead>
