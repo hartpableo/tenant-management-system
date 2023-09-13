adc78036280af0a886fcdde591f04dd060cb4a79
Hart <hart.hpwebdev@gmail.com>
2023-09-13 14:06:44 +0800

    Feature: Export Tenants Data to CSV

--- app/Http/controllers/Tenants/export-data.php
+++ app/Http/controllers/Tenants/export-data.php
@@ -1,14 +1,12 @@
 <?php
 
-ob_start();
-
 use Core\App;
 use Core\Database;
 use Core\Session;
 
 $db = App::resolve(Database::class);
 
-$tenantsData = $db->query('select * from tenants order by id desc')->findAll();
+$tenantsData = $db->query('select id, name, email, contact, room, rent_start from tenants order by id asc')->findAll();
 
 if (!isset($_POST) || !count($tenantsData)) {
   Session::flash('errors', [
@@ -18,65 +16,26 @@ if (!isset($_POST) || !count($tenantsData)) {
   redirect();
 }
 
-/**
- * ============
- * Markup Start
-*/
-$markup = "
-<table class='table table-striped border'>
-      <thead>
-        <tr>
-          <th scope='col'>ID</th>
-          <th scope='col'>&nbsp;<span class='visually-hidden'>Profile Image</span></th>
-          <th scope='col'>Name</th>
-          <th scope='col'>Email Address</th>
-          <th scope='col'>Contact Number</th>
-          <th scope='col'>Room</th>
-          <th scope='col'>Rent Start</th>
-        </tr>
-      </thead>
-      <tbody>
-";
-
-foreach ($tenantsData as $tenant) :
-
-  $profileImage = getProfileImage($tenant['profile_image']);
-  $rentStart = convertDate($tenant['rent_start'], 'M d, Y');
-
-  $markup .= "
-  <tr>
-    <th scope='row'>{$tenant['id']}</th>
-    <td>{$profileImage}</td>
-    <td>{$tenant['name']}</td>
-    <td>{$tenant['email']}</td>
-    <td>{$tenant['contact']}</td>
-    <td>{$tenant['room']}</td>
-    <td>{$rentStart}</td>
-  </tr>
-  ";
-
-endforeach;
-
-$markup .= "
-  </tbody>
-</table>
-";
-/**
- * Markup End
- * ==========
-*/
-
 $datenow = date('M-Y--') . time();
-ob_end_clean();
+$columnNames = [
+  'ID',
+  'Name',
+  'Email Address',
+  'Contact Number',
+  'Room',
+  'Rent Started'
+];
 
-// header("Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
-// header("Content-Disposition: attachment; filename:Tenants-Data--{$datenow}.xls");
 header("Content-Type: application/csv");
 header("Content-Disposition: attachment; filename=Tenants-Data--{$datenow}.csv");
 header("Pragma: no-cache");
 
 $outputFile = fopen('php://output', 'r+');
 
+// Write column names to the file
+fputcsv($outputFile, $columnNames);
+
+// Write table data to the file
 foreach ($tenantsData as $tenant) :
   fputcsv($outputFile, $tenant);
 endforeach;
